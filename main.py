8fec28eaf1b08fd6541b00fc9931501d48a4a0a6
SkareMartin22 <815360@mystma.org>
2021-12-22 18:16:31 +0000

    1

--- main.py
+++ main.py
@@ -18,16 +18,16 @@ charisma= int(input("How charasmatic are you? (1-100)\n>"))
 if charisma > 100 or charisma < 0 or charisma > points:
     print ("You tried that same thing for strength, didn't you? I'm losing my patience. You LOSE!")
     exit()
-points= points - strength - charisma
+points= points - charisma
 print ("You get all of the ladies, don't you. CHARISMA SET TO " + str(charisma))
 print ("You have " + str(points) + " skill points remaining")
 
-dexterity= int(input("How dexterity are you? (1-100)\n>"))
+dexterity= int(input("How is your dexterity? (1-100)\n>"))
 
 if dexterity > 100 or dexterity < 0 or dexterity > points:
     print("My patience is fading fast. You can't do that! You LOSE")
     exit()
-points= points - strength - charisma - dexterity
+points= points - dexterity
 print("Awe yes, you are a sneaky boi. DEXTERITY SET TO " + str(dexterity))
 print ("You have " + str(points) + " skill points remaining.")
 
@@ -36,7 +36,7 @@ intelligence= int(input("How intelligent are you? (1-100)\n>"))
 if intelligence > 100 or intelligence < 0 or intelligence > points:
     print("If it's your first time trying, you're fine just try again. However, if you tried FOUR TIMES, you need help and therapy. You can't do that! You LOSE")
     exit()
-points= points - strength - charisma - dexterity - intelligence
+points= points - intelligence
 print("Awe quite the scholar you are. INTELLIGENCE SET TO " + str(intelligence))
 print ("You have " + str(points) + " skill points remaining.")
 
@@ -45,7 +45,7 @@ magic= int(input("How much expirence in the magical arts have you gathered? (1-1
 if magic > 100 or magic < 0 or magic > points:
     print("If it's your first time trying, you're not the problem, just try again. However, if you have successfully annoyed me EVERY SINGLE TIME, you need a lot more then therapy. You LOSE")
     exit()
-points= points - strength - charisma - dexterity - intelligence - magic
+points= points - magic
 print("Awe you must have done quite a few childrens parties. MAGICAL ABILITY SET TO " + str(magic))
 print ("You have should have 0 points remaining, but if you were STUPID, this is how much you wasted." + str(points) + " skill points remaining.")
 print (" ")
@@ -63,13 +63,20 @@ choice= input(">")
 if choice == "1":
     roll = random.randrange(0, strength)
     if roll > 20:
-        print("The wall shatters in aweof your divine strength.")
+        print("The wall shatters in awe of your divine strength.")
         
     else:
         print("Your fist shatters in awe of the wall's divine strength")
         exit()
 elif choice == "2":
-    print("cool")
+    roll = random.randrange(0, charisma)
+    if roll > 30:
+        print("The wall is moved by your compelling arguement and lets you pass as it cries. ")
+
+    else:
+        print("It's a wall, what did you expect? Try again idiot.")
+
+
 elif choice == "3":
     print("cool")
 elif choice == "4":
